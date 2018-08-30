00d9a4dd3f8f6da4501ac7fb29b6d34ef8f4a13f
maurizioterreni <maurizio.terreni@live.it>
2018-08-30 12:17:35 +0200

    change request

--- gui/js/dashboard.js
+++ gui/js/dashboard.js
@@ -1,4 +1,4 @@
-function loadCard() {
+/*
   var PythonShell = require('python-shell');
   PythonShell.run('/python/getWeatherStation.py', function (err, results) {
     if (err) throw err;
@@ -8,6 +8,40 @@ function loadCard() {
 
 }
 
+*/
+var nodeConsole = require('console');
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+var request = require('request');
+
+function loadCard() {
+  myConsole.log('i am here');
+/*  const request = net.request({
+      method: 'GET',
+      protocol: 'http:',
+      hostname: 'maurizioterreni.altervista.org',
+      path: '/rest/services/weatherstation/read.php'
+    })
+
+
+  request.on('response', (response) => {
+      myConsole.log(`STATUS: ${response.statusCode}`);
+      response.on('error', (error) => {
+        myConsole.log(`ERROR: ${JSON.stringify(error)}`)
+      })
+    })*/
+
+
+    request('http://maurizioterreni.altervista.org/rest/services/weatherstation/read.php', function (error, response, body) {
+      //myConsole.log('error:', error); // Print the error if one occurred
+    //  myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      //myConsole.log('body:', body); // Print the HTML for the Google homepage.
+      if(response.statusCode == 200){
+        document.getElementById('containerDashboard').innerHTML = "<div class=\"alert alert-success\" role=\"alert\"><strong>" + response.statusCode  + "! </div>";
+      }else{
+        document.getElementById('containerDashboard').innerHTML = "<div class=\"alert alert-danger\" style=\"margin-top:20px\" role=\"alert\"><strong>Error " + response.statusCode + "! I can't read data.</div>";
+      }
+    });
+  }
 
 
 
