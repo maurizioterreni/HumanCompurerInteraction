363482ce445e53862d74c9035fb0a656a7191366
Maurizio Terreni <maurizio.terreni@live.it>
2018-08-30 15:17:04 +0200

    popolata dashboard

--- gui/js/dashboard.js
+++ gui/js/dashboard.js
@@ -14,29 +14,25 @@ var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 var request = require('request');
 
 function loadCard() {
-  myConsole.log('i am here');
-/*  const request = net.request({
-      method: 'GET',
-      protocol: 'http:',
-      hostname: 'maurizioterreni.altervista.org',
-      path: '/rest/services/weatherstation/read.php'
-    })
-
-
-  request.on('response', (response) => {
-      myConsole.log(`STATUS: ${response.statusCode}`);
-      response.on('error', (error) => {
-        myConsole.log(`ERROR: ${JSON.stringify(error)}`)
-      })
-    })*/
-
-
+    //document.getElementById('containerDashboard').innerHTML = "<div class=\"container\" id=\"containerDashboard\"><div style=\"width:100%; text-align: center; margin-top:20px;\"><img src=\"../images/loading-icon.gif\"/></div></div>";
+    myConsole.log('i am here');
     request('http://maurizioterreni.altervista.org/rest/services/weatherstation/read.php', function (error, response, body) {
       //myConsole.log('error:', error); // Print the error if one occurred
     //  myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       //myConsole.log('body:', body); // Print the HTML for the Google homepage.
       if(response.statusCode == 200){
-        document.getElementById('containerDashboard').innerHTML = "<div class=\"alert alert-success\" role=\"alert\"><strong>" + response.statusCode  + "! </div>";
+        var obj = JSON.parse(body.replace(/ 0+(?![\. }])/g, ' '));
+        var html = '';
+        for(var k in obj) {
+         if(k%2 == 0){
+           html += '<div class="row">';
+           html += '<div class="col-md-6"><div class="card"><div class="card-header"> '+obj[k].description+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'+obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
+         }else{
+           html += '<div class="col-md-6"><div class="card"><div class="card-header"> '+obj[k].description+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'+obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
+           html += '</div>';
+         }
+        }
+        document.getElementById('containerDashboard').innerHTML = html;
       }else{
         document.getElementById('containerDashboard').innerHTML = "<div class=\"alert alert-danger\" style=\"margin-top:20px\" role=\"alert\"><strong>Error " + response.statusCode + "! I can't read data.</div>";
       }
