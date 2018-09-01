afc051d704629e336ecfa9e5e55e2f022d64c5d5
maurizioterreni <maurizio.terreni@live.it>
2018-09-01 11:52:57 +0200

    add modal

--- gui/js/dashboard.js
+++ gui/js/dashboard.js
@@ -1,92 +1,52 @@
-/*
-  var PythonShell = require('python-shell');
-  PythonShell.run('/python/getWeatherStation.py', function (err, results) {
-    if (err) throw err;
-    console.log(results.toString());
-    document.getElementById('containerDashboard').innerHTML = results.toString();
-  });
-
-}
-
-*/
+'use strict'
 var nodeConsole = require('console');
 var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
 var request = require('request');
+window.$ = window.jQuery = require('jquery');
+window.Tether = require('tether');
+window.Bootstrap = require('bootstrap');
 
 $("#buttonOpenMaps").on("click", function(){
   $("#mapsModal").modal('show');
 });
 
-function loadCard() {
-    //document.getElementById('containerDashboard').innerHTML = "<div class=\"container\" id=\"containerDashboard\"><div style=\"width:100%; text-align: center; margin-top:20px;\"><img src=\"../images/loading-icon.gif\"/></div></div>";
-    //myConsole.log('i am here');
+
+$( document ).ready(function() {
+  //document.getElementById('containerDashboard').innerHTML = "<div class=\"container\" id=\"containerDashboard\"><div style=\"width:100%; text-align: center; margin-top:20px;\"><img src=\"../images/loading-icon.gif\"/></div></div>";
+  //myConsole.log('i am here');
     request('http://maurizioterreni.altervista.org/rest/services/weatherstation/read.php', function (error, response, body) {
-      //myConsole.log('error:', error); // Print the error if one occurred
-    //  myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      //myConsole.log('body:', body); // Print the HTML for the Google homepage.
-      if(response.statusCode == 200){
-        var obj = JSON.parse(body.replace(/ 0+(?![\. }])/g, ' '));
-        var html = '';
-        for(var k in obj) {
-          div = '<div class="col-md-6"><div class="card"><div class="card-header"> '
-               + obj[k].description +'<br></div><div class="card-body"><h4 class="text-center"> '
-               + 'Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'
-               +obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><button type="button" id="buttonOpenMaps" data-toggle="modal" data-target="#mapsModal" class="btn btn-primary" href="#">'
-               +'Maps<br></button></div><div class="col-md-6"><a class="btn btn-primary" >Open<br></a></div></div></div></div></div>';
-           if(k%2 == 0){
-             html += '<div class="row">';
-             html += div;
-           }else{
-             html += div;
-             html += '</div>';
-           }
+        //myConsole.log('error:', error); // Print the error if one occurred
+        //myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        //myConsole.log('body:', body); // Print the HTML for the Google homepage.
+        if(response.statusCode == 200){
+          var obj = JSON.parse(body.replace(/ 0+(?![\. }])/g, ' '));
+          var html = '';
+          for(var k in obj) {
+            var div = '<div class="col-md-6"><div class="card"><div class="card-header"> '
+                 + obj[k].description +'<br></div><div class="card-body"><h4 class="text-center"> '
+                 + 'Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'
+                 +obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><button type="button" id="buttonOpenMaps" data-toggle="modal" data-target="#mapsModal" class="btn btn-primary" href="#">'
+                 +'Maps<br></button></div><div class="col-md-6"><a class="btn btn-primary" >Open<br></a></div></div></div></div></div>';
+             if(k%2 == 0){
+               html += '<div class="row">';
+               html += div;
+             }else{
+               html += div;
+               html += '</div>';
+             }
+          }
+          $('#containerDashboard').html(html);
+        }else{
+          $('#containerDashboard').html("<div class=\"alert alert-danger\" style=\"margin-top:20px\" role=\"alert\"><strong>Error " + response.statusCode + "! I can't read data.</div>");
         }
-        document.getElementById('containerDashboard').innerHTML = html;
-      }else{
-        document.getElementById('containerDashboard').innerHTML = "<div class=\"alert alert-danger\" style=\"margin-top:20px\" role=\"alert\"><strong>Error " + response.statusCode + "! I can't read data.</div>";
-      }
-    });
-  }
-
-
-  $( document ).ready(function() {
-      myConsole.log( "ready!" );
   });
+});
 
 
-
-/*
-
-function loadCard(){
-var str = '[{"id": "1","description": "WeatherStation Empoli","longitude": "10.951802242615486","latitude": "43.69489556632679","image": "https://images.pond5.com/meteorological-weatherstation-anemometer-footage-000591947_iconl.jpeg","sensors": "[1,2,3,4,5,6,7]"},{"id": "2","description": "Stazione Meteo di User1","longitude": "10.93473908437511","latitude": "43.791581260296006","image": "http://zwaveit.com/image/cache/data/65Riego/69%20Jardin/qubino-station-meteo-z-wave-zmnhzd1-600x600.jpg","sensors": "[8,9,10,11,12,13]"},{"id": "4","description": "Stazione Meteo Santa Marta","longitude": "11.2527708005249","latitude": "43.79850144596702","image": "http://ppsystems.com/wp-content/uploads/MetPak-Pro.jpg","sensors": "[]"}]';
-var obj = JSON.parse(str.replace(/ 0+(?![\. }])/g, ' '));
-var html = '';
-for(var k in obj) {
- if(k%2 == 0){
-   html += '<div class="row">';
-   html += '<div class="col-md-6"><div class="card"><div class="card-header"> '+obj[k].description+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'+obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
- }else{
-   html += '<div class="col-md-6"><div class="card"><div class="card-header"> '+obj[k].description+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'+obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
-   html += '</div>';
- }
-}
-document.getElementById('containerDashboard').innerHTML = html;
-console.log(html);
-}
-
-//function loadCard() {
-
-/*  var i;
-  var html = '';
-  for (i = 0; i < 10; i++) {
-    if(i%2 == 0){
-      html += '<div class="row">';
-      html += '<div class="col-md-6"><div class="card"><div class="card-header"> Stazione meteo '+i+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto" style="margin:auto;" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" width="200" height="200"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
-    }else{
-      html += '<div class="col-md-6"><div class="card"><div class="card-header"> Stazione meteo '+i+'<br></div><div class="card-body"><h4 class="text-center">Card title</h4><img class="img-fluid d-block mx-auto" style="margin:auto;" src="https://pingendo.com/assets/photos/wireframe/photo-1.jpg" width="200" height="200"><div class="row text-center my-4"><div class="col-md-6 text-center"><a class="btn btn-primary" href="#">Maps<br></a></div><div class="col-md-6"><a class="btn btn-primary" href="#">Open<br></a></div></div></div></div></div>';
-      html += '</div>';
-    }
-  }*/
-
-
-//}
+$(function() {
+    $("#map").googleMap({
+      zoom: 10, // Initial zoom level (optional)
+      coords: [48.895651, 2.290569], // Map center (optional)
+      type: "ROADMAP" // Map type (optional)
+    });
+})
--- main.js
+++ main.js
@@ -1,59 +1,55 @@
+'use strict'
+
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
+const globalShortcut = electron.globalShortcut
+const os = require('os')
+const path = require('path')
+const config = require(path.join(__dirname, 'package.json'))
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
+app.setName(config.productName)
+var mainWindow = null
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    backgroundColor: 'lightgray',
+    title: config.productName,
+    show: false,
+    width: 1440,
+    height: 990,
+    webPreferences: {
+      nodeIntegration: true,
+      defaultEncoding: 'UTF-8'
+    }
+  })
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+  mainWindow.loadURL(`file://${__dirname}/gui/dashboard.html`)
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1024, height: 720, frame: true})
+  // Enable keyboard shortcuts for Developer Tools on various platforms.
+  let platform = os.platform()
+  if (platform === 'darwin') {
+    globalShortcut.register('Command+Option+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  } else if (platform === 'linux' || platform === 'win32') {
+    globalShortcut.register('Control+Shift+I', () => {
+      mainWindow.webContents.openDevTools()
+    })
+  }
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'gui/dashboard.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.setMenu(null)
+    mainWindow.show()
+  })
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.onbeforeunload = (e) => {
+    // Prevent Command-R from unloading the window contents.
+    e.returnValue = false
+  }
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-
-    app.quit()
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => { app.quit() })
