7cf0f064dc12432fc709b24affd8dde7d0f604a0
maurizioterreni <maurizio.terreni@live.it>
2018-09-02 22:56:01 +0200

    commit

--- gui/js/dashboard.js
+++ gui/js/dashboard.js
@@ -6,17 +6,12 @@ window.$ = window.jQuery = require('jquery');
 window.Tether = require('tether');
 window.Bootstrap = require('bootstrap');
 
-$("#buttonOpenMaps").on("click", function(){
-  $("#mapsModal").modal('show');
-});
-
-
 $( document ).ready(function() {
   //document.getElementById('containerDashboard').innerHTML = "<div class=\"container\" id=\"containerDashboard\"><div style=\"width:100%; text-align: center; margin-top:20px;\"><img src=\"../images/loading-icon.gif\"/></div></div>";
   //myConsole.log('i am here');
     request('http://maurizioterreni.altervista.org/rest/services/weatherstation/read.php', function (error, response, body) {
-        //myConsole.log('error:', error); // Print the error if one occurred
-        //myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        myConsole.log('error:', error); // Print the error if one occurred
+        myConsole.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         //myConsole.log('body:', body); // Print the HTML for the Google homepage.
         if(response.statusCode == 200){
           var obj = JSON.parse(body.replace(/ 0+(?![\. }])/g, ' '));
@@ -26,7 +21,7 @@ $( document ).ready(function() {
                  + obj[k].description +'<br></div><div class="card-body"><h4 class="text-center"> '
                  + 'Card title</h4><img class="img-fluid d-block mx-auto imgWeather" style="margin:auto;" src="'
                  +obj[k].image+'"><div class="row text-center my-4"><div class="col-md-6 text-center"><button type="button" id="buttonOpenMaps" data-toggle="modal" data-target="#mapsModal" class="btn btn-primary" href="#">'
-                 +'Maps<br></button></div><div class="col-md-6"><a class="btn btn-primary" >Open<br></a></div></div></div></div></div>';
+                 +'Maps</button></div><div class="col-md-6"><button type="button" id="buttonOpenSensor" href="#" class="btn btn-primary">Open</button></div></div></div></div></div>';
              if(k%2 == 0){
                html += '<div class="row">';
                html += div;
@@ -40,8 +35,17 @@ $( document ).ready(function() {
           $('#containerDashboard').html("<div class=\"alert alert-danger\" style=\"margin-top:20px\" role=\"alert\"><strong>Error " + response.statusCode + "! I can't read data.</div>");
         }
   });
-});
 
+
+  $("#buttonOpenMaps").on("click", function(){
+    $("#mapsModal").modal('show');
+  });
+
+  $('#buttonOpenSensor').on("click", function(){
+    $('#containerDashboard').html(ciao);
+  });
+
+});
 
 $(function() {
     $("#map").googleMap({
