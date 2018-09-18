26f613ee751a131308d97daba820bce9295b881d
maurizioterreni <maurizio.terreni@live.it>
2018-09-18 22:14:28 +0200

    fix daylight

--- src/app/layout/sensor/daylight/daylight.component.ts
+++ src/app/layout/sensor/daylight/daylight.component.ts
@@ -9,15 +9,15 @@ import { Sensor } from '../../../models/sensor/sensor';
 })
 export class DaylightCardComponent implements OnInit, OnChanges {
   now = Date.now();
-  //sunset : Date;
-  //sunrise : Date;
+  sunset : any;
+  sunrise : any;
   constructor() {
     //this.d = new Date(date);
   //  this.sunrise = new Date(this.now.getFullYear() + '', this.now.getMonth() + '', this.now.getDay() + '', '6', '50');
   //  this.sunset = new Date(this.now.getFullYear() + '', this.now.getMonth() + '', this.now.getDay() + '', '19', '10');
+    this.sunrise = new Date(2018, 9, 18, 6, 50);
+    this.sunset = new Date(2018, 9, 18, 19, 10);
 
-  //  this.sunrise = new Date('2018','09', '18', '6', '50');
-  //  this.sunset = new Date('2018','09', '18', '19', '10');
   }
 
   ngOnInit() {
@@ -34,8 +34,19 @@ export class DaylightCardComponent implements OnInit, OnChanges {
   }*/
 
   public getRotation() : string{
-  //  let num = this.now.getTime() + 0;// - this.getMillies(this.sunrise);
-//    console.log(num);
-    return '100';
+    var num = this.now - this.sunrise;
+    num = (((num / (this.sunset - this.sunrise)) * 172) - 49);
+
+    if(num > 123){
+      num = 123;
+    }
+
+    if(num < -49){
+      num = -49;
+    }
+
+    return String(num);
   }
+
+
 }
