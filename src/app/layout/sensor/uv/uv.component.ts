bf066fe2007c98d69fb8255754b18a9cf80d2335
maurizioterreni <maurizio.terreni@live.it>
2018-09-24 13:06:00 +0200

    add uv data

--- src/app/layout/sensor/uv/uv.component.ts
+++ src/app/layout/sensor/uv/uv.component.ts
@@ -1,21 +1,57 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { Sensor } from '../../../models/sensor/sensor';
+import { Measure } from '../../../models/measure/measure';
+import { MeasureService } from '../../../services/measure/measure.service';
 
 @Component({
   selector: 'uv-card',
   templateUrl: './uv.html',
   styleUrls: ['./uv.css'],
-  providers: []
+  providers: [ MeasureService ]
 })
 export class UvCardComponent implements OnInit, OnChanges {
   @Input() sensors: Sensor[];
 
-  constructor() {
+  measureUv: Measure;
+//  measureLux: Measure;
 
+  constructor(measureService : MeasureService) {
+    setInterval(() => {
+        this.getCurrentData();
+     }, 900000);//15 minuti
   }
 
   ngOnInit() {
 
+  }
+
+  private getCurrentData(){
+    this.measureService.getLastMeasure(this.sensors[0].id)
+      .subscribe((result: any) => {
+          this.measureUv = <Measure> result;
+      });
+/*    this.measureService.getLastMeasure(this.sensors[1].id)
+      .subscribe((result: any) => {
+          this.measureUv = <Measure> result;
+      });*/
+  }
+
+  public getMeasureValue(measure: Measure) {
+    if(measure == null){
+      return 'NaN';
+    }
+
+    return Math.round(measure.quantity);
+  }
+
+  public getUvRange(){
+    if(this.measureUv == null){
+      return '0';
+    }
+
+    let value = Number(this.measurePressure.quantity);
+    let num = value;
+    return (((num / 11) * 100)) + '';
   }
 
   ngOnChanges(changes: SimpleChanges) {
