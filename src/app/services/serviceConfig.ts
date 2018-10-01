fea21d1e0ed20f4fce1a257b20346097e60be581
maurizioterreni <maurizio.terreni@live.it>
2018-10-01 21:45:08 +0200

    bug fix

--- src/app/services/serviceConfig.ts
+++ src/app/services/serviceConfig.ts
@@ -1,10 +1,10 @@
 export class ServiceConfig {
     private protocol = "http";
-    //private host = "80.211.86.68";
-    private host = "localhost";
+    private host = "80.211.86.68";
+    //private host = "localhost";
     private port = "8080";
-    //private url = "WeatherApi/rest/1.0/";
-    private url = "WeatherSt-0.0.1-SNAPSHOT/rest/1.0/";
+    private url = "WeatherApi/rest/1.0/";
+    //private url = "WeatherSt-0.0.1-SNAPSHOT/rest/1.0/";
 
     constructor() {}
 
